1d8a24a2240ed8340e72404825c0cdf1c819156d
Alen Boric <boricalen@live.com>
2023-02-25 20:18:32 +0100

    The StreamBuilder Widget

--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   Widget emailField() {
-    return TextField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        hintText: 'you@example.com',
-        labelText: 'Email Address',
-      ),
-    );
+    return StreamBuilder(
+        stream: bloc.email,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'you@example.com',
+              labelText: 'Email Address',
+              errorText: snapshot.hasError ? snapshot.error.toString() : null,
+            ),
+          );
+        });
   }
 
   Widget passwordField() {
