124357b9afec3c03bca95c565d237d2f1b855e7a
Alen Boric <boricalen@live.com>
2023-02-27 21:26:24 +0100

    The Provider's Constructor

--- login_bloc/lib/src/blocs/provider.dart
+++ login_bloc/lib/src/blocs/provider.dart
@@ -4,10 +4,15 @@ import 'bloc.dart';
 class Provider extends InheritedWidget {
   final bloc = Bloc();
 
-  Provider({required super.child});
+  Provider({required Key key, required Widget child})
+      : super(key: key, child: child);
+
+  // Provider({required super.child});
   bool updateShouldNotify(_) => true;
 
   static Bloc of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<Provider>()!.bloc;
+    return (context.dependOnInheritedWidgetOfExactType<Provider>(
+            aspect: Provider))!
+        .bloc;
   }
 }
